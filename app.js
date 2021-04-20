3b46783324e64514f112a00e7cb21f31d8169680
rachurion <rachurion@gmail.com>
2021-04-20 11:59:55 +0200

    delete

--- app.js
+++ app.js
@@ -1,27 +1,27 @@
+const { nanoid } = require('nanoid');
+
 const BBDD = [
 	{
 		urlimagen:
 			'https://i.picsum.photos/id/237/536/354.jpg?hmac=i0yVXW1ORpyCZpQ-CknuyV-jbtU7_x9EBQVhvT5aRr0',
 		date: '2020-04-18',
 		titulo: 'Black dog',
-		color: [ 42, 35, 32 ]
+		color: [42, 35, 32],
+		id: nanoid(),
 	},
 	{
 		urlimagen:
 			'https://i.picsum.photos/id/490/200/300.jpg?hmac=8hYDsOwzzMCthBMYMN9bM6POtrJfVAmsvamM2oOEiF4',
 		date: '2021-04-19',
 		titulo: 'Bol',
-		color: [43, 34, 28]
+		color: [43, 34, 28],
+		id: nanoid(),
 	},
 ];
 
 let BBDDsorted = BBDD.sort((a, b) => new Date(b.date) - new Date(a.date));
 
-const ColorThief = require('colorthief');
-
-
-
-
+const { getColorFromURL } = require('color-thief-node');
 
 const express = require('express');
 const app = express();
@@ -42,27 +42,22 @@ app.get('/add-image', (req, res) => {
 app.post('/add-image', (req, res) => {
 	if (urlExist(req.body.urlimagen)) {
 		res.send('This URL already exists');
-		return
+		return;
 	}
 	const image = {
 		urlimagen: req.body.urlimagen,
 		date: req.body.date,
 		titulo: req.body.titulo,
-
+		id: nanoid(),
 	};
 
-	ColorThief.getColor(image.urlimagen)
-	.then(color => { image.color =color })
-	.then( () => {
+	(async () => {
+		const dominantColor = await getColorFromURL(image.urlimagen);
+		image.color = dominantColor;
 		BBDD.push(image);
 		BBDDSorted = BBDD.sort((a, b) => new Date(b.date) - new Date(a.date));
 		res.redirect('/');
-	})
-	.catch(err => { console.log(err) })
-
-	
-	
-	
+	})();
 });
 
 app.listen(3000);
